0724543b72586fd4c6c099d898711f7d973958f2
Ismail <shaikhnagismail@gmail.com>
2025-08-12 17:24:51 +0200

    refactor: simplify AllFavoritesScreen by replacing custom floating button with FabButton component

--- src/app/(protected)/index.jsx
+++ src/app/(protected)/index.jsx
@@ -1,24 +1,19 @@
 import React from "react";
-import { View, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Ionicons } from "@expo/vector-icons";
 
 import FavoriteList from "../../components/allFavoritesScreen/FavoriteList";
+import FabButton from "../../components/allFavoritesScreen/FabButton";
 
 import { favorites } from "../../../data/favorites";
 
 export default function AllFavoritesScreen() {
   return (
-    <SafeAreaView className="flex-1" edges={["top", "right", "left"]}>
+    <SafeAreaView className="flex-1 px-4" edges={["top", "right", "left"]}>
       {/* Favorites Grid */}
       <FavoriteList favorites={favorites} />
 
       {/* Floating Add Button */}
-      <View className="absolute bottom-8 right-6">
-        <TouchableOpacity className="h-14 w-14 items-center justify-center rounded-full bg-purple-600 shadow-lg">
-          <Ionicons name="add" size={28} color="white" />
-        </TouchableOpacity>
-      </View>
+      <FabButton />
     </SafeAreaView>
   );
 }
