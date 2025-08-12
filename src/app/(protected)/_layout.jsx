20d86b2af80a4f5eae249ad329eb8a9eaf51a210
Ismail <shaikhnagismail@gmail.com>
2025-08-12 15:39:28 +0200

    feat: add header options for favorites screen in ProtectedLayout

--- src/app/(protected)/_layout.jsx
+++ src/app/(protected)/_layout.jsx
@@ -5,7 +5,16 @@ import { Stack } from "expo-router";
 export default function ProtectedLayout() {
   return (
     <Stack initialRouteName="index">
-      <Stack.Screen name="index" />
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "All Favorites",
+          headerStyle: { backgroundColor: "#f4511e" },
+          headerTintColor: "#fff",
+          headerTitleStyle: { fontWeight: "bold" },
+          headerTitleAlign: "center",
+        }}
+      />
     </Stack>
   );
 }
