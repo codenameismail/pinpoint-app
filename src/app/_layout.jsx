9567564dbf0484ee4cb737d0e6eb073b8877679e
Ismail <shaikhnagismail@gmail.com>
2025-08-13 12:11:10 +0200

    feat: wrap RootLayout in SafeAreaProvider for improved layout handling

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -3,16 +3,17 @@ import React from "react";
 
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <>
+    <SafeAreaProvider>
       <StatusBar style="dark" />
       <Stack
         screenOptions={{
           headerShown: false,
         }}
       />
-    </>
+    </SafeAreaProvider>
   );
 }
