d839e0f6ed9faf7f53ccfa72d0501985fffe5f49
Ismail <shaikhnagismail@gmail.com>
2025-08-14 19:01:23 +0200

    feat: replace LocationSection with LocationPicker and update form handling in AddFavoritePlaceScreen

--- src/app/(protected)/favorite/new.jsx
+++ src/app/(protected)/favorite/new.jsx
@@ -6,7 +6,7 @@ import { View, ScrollView, Modal } from "react-native";
 
 import FormField from "../../../components/FormField";
 import ImagePicker from "../../../components/newFavoriteScreen/ImagePicker";
-import LocationSection from "../../../components/newFavoriteScreen/LocationSection";
+import LocationPicker from "../../../components/newFavoriteScreen/LocationPicker";
 import SaveButton from "../../../components/newFavoriteScreen/SaveButton";
 import HeaderBar from "../../../components/HeaderBar";
 import { ImageSection } from "../../../components/newFavoriteScreen/ImageSection";
@@ -16,20 +16,29 @@ import { useAddFavoritePlace } from "../../../hooks/useAddFavoritePlace";
 export default function AddFavoritePlaceScreen() {
   // Custom hook to manage the state and logic for adding a favorite place
   const {
-    inputTitle,
-    setTitleInput,
-    pickedImageUri,
-    handleImagePicked,
-    handleUseCurrentLocation,
+    // Form state
+    title,
+    setTitle,
+    imageUri,
+    location,
+    description,
+    setDescription,
+
+    // UI state
+    isSubmitting,
     isPickerVisible,
     setPickerVisible,
-    handleSave,
+
+    // Handlers
+    handleImageSelected,
+    handleLocationSelected,
+    handleResetLocation,
+    handleSubmit,
   } = useAddFavoritePlace();
 
   return (
     <SafeAreaView className="flex-1 bg-white" edges={["top"]}>
       <StatusBar style="dark" />
-      {/* Header */}
       <HeaderBar title="Add New Place" />
 
       <ScrollView
@@ -37,15 +46,15 @@ export default function AddFavoritePlaceScreen() {
         contentContainerStyle={{
           paddingBottom: 40,
         }}
-        keyboardShouldPersistTaps="never"
+        keyboardShouldPersistTaps="handled"
         showsVerticalScrollIndicator={false}
       >
         {/* Title Input */}
         <View className="mb-6">
           <FormField
             label={"Title"}
-            onChangeText={setTitleInput}
-            value={inputTitle}
+            value={title}
+            onChangeText={setTitle}
             placeholder={"e.g. My Favorite Coffee Shop"}
             inputProps={{
               returnKeyType: "done",
@@ -55,21 +64,47 @@ export default function AddFavoritePlaceScreen() {
 
         {/* Image */}
         <ImageSection
-          pickedImageUri={pickedImageUri}
+          pickedImageUri={imageUri}
           onPickImage={() => setPickerVisible(true)}
         />
 
         {/* Location */}
-        <LocationSection onUseCurrentLocation={handleUseCurrentLocation} />
+        <LocationPicker
+          selectedLocation={location}
+          onLocationSelected={handleLocationSelected}
+          onResetLocation={handleResetLocation}
+        />
+
+        {/* <LocationSection onUseCurrentLocation={handleLocationSelected} /> */}
+
+        {/* Description */}
+        <View className="mb-8 pt-9">
+          <FormField
+            label="Description (Optional)"
+            value={description}
+            onChangeText={setDescription}
+            placeholder="Tell us about this place..."
+            inputProps={{
+              multiline: true,
+              numberOfLines: 4,
+              textAlignVertical: "top",
+              style: { height: 100, paddingTop: 12 },
+            }}
+          />
+        </View>
 
         {/* Save Button */}
-        <SaveButton onPress={handleSave} />
+        <SaveButton
+          onPress={handleSubmit}
+          disabled={isSubmitting}
+          isLoading={isSubmitting}
+        />
       </ScrollView>
 
       {/* --- MODAL FOR THE IMAGE PICKER --- */}
       <Modal visible={isPickerVisible} animationType="slide">
         <ImagePicker
-          onImagePicked={handleImagePicked}
+          onImagePicked={handleImageSelected}
           onCancel={() => setPickerVisible(false)}
         />
       </Modal>
