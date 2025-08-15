07dfca4c920a05019d2a2b91e04e1e97228b3fa6
Ismail <shaikhnagismail@gmail.com>
2025-08-15 18:52:26 +0200

    refactor: enhance AllFavoritesScreen structure for improved readability

--- src/app/(protected)/index.jsx
+++ src/app/(protected)/index.jsx
@@ -4,9 +4,11 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import FavoriteList from "../../components/allFavoritesScreen/FavoriteList";
 import FabButton from "../../components/allFavoritesScreen/FabButton";
 
-import { favorites } from "../../data/favorites";
+import { useFavoritesStore } from "../../store/useFavoritesStore";
 
 export default function AllFavoritesScreen() {
+  const favorites = useFavoritesStore((state) => state.favorites);
+
   return (
     <SafeAreaView className="flex-1 px-4" edges={["top", "right", "left"]}>
       {/* Favorites Grid */}
